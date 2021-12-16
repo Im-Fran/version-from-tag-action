0b810d61505d492492df1d4be3736579250d4511
Francisco Solis <fran@theprogramsrc.xyz>
2021-12-16 18:39:46 -0300

    Yet another fix, oops * Fixed RemoveFirstCharacter code

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ try {
         const tagName = process.env.GITHUB_REF_NAME
         const removeFirstCharacter = core.getInput('remove-first-character') || 'none'
         const variableName = core.getInput('version-variable-name') || 'VERSION'
-        core.exportVariable(variableName, removeFirstCharacter ? tagName.substring(tagName.indexOf(removeFirstCharacter)) : tagName)
+        let version;
+        if(removeFirstCharacter !== 'none') {
+            const indexOf = tagName.indexOf(removeFirstCharacter)
+            version = tagName.substring(indexOf, indexOf + removeFirstCharacter.length)
+        }else{
+            version = tagName
+        }
+        core.exportVariable(variableName, version)
     }
 }catch (e) {
     core.setFailed(e.message);
