f4476b1dd701024c639cba0ed033a372b0794c6b
Francisco Solis <fran@theprogramsrc.xyz>
2021-12-16 18:25:50 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    if(process.env.GITHUB_REF_TYPE === 'tag') {
+        const tagName = process.env.GITHUB_REF_NAME
+        const removeFirstCharacter = core.getInput('remove-first-character') || 'none'
+        const variableName = core.getInput('version-variable-name') || 'VERSION'
+        core.exportVariable(variableName, removeFirstCharacter ? tagName.substring(1) : tagName)
+    }
+}catch (e) {
+    core.setFailed(e.message);
+}
