6263cfebdd8c24bf4b8ed2f77d12fcd2bdc152a1
Francisco Solis <fran@theprogramsrc.xyz>
2021-12-16 18:25:50 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
         const tagName = process.env.GITHUB_REF_NAME
         const removeFirstCharacter = core.getInput('remove-first-character') || 'none'
         const variableName = core.getInput('version-variable-name') || 'VERSION'
-        core.exportVariable(variableName, removeFirstCharacter ? tagName.substring(1) : tagName)
+        core.exportVariable(variableName, removeFirstCharacter ? tagName.replace(removeFirstCharacter, '') : tagName)
     }
 }catch (e) {
     core.setFailed(e.message);
